b2963c352ab19a59e0c1482910039c51008ccfab
Matt Leuterio <leuterio.matteo@gmail.com>
2025-08-27 08:55:27 +0200

    feat: favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,40 @@ const kapakana = Kapakana({ subsets: ["latin"], weight: ["400"] });
 export const metadata = {
   title: "La città più bella del mondo",
   description: "Cosenza - La città più bella del mondo",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      { rel: "android-chrome", url: "/android-chrome-192x192.png" },
+      { rel: "android-chrome", url: "/android-chrome-512x512.png" },
+    ],
+  },
+  openGraph: {
+    title: "La città più bella del mondo",
+    description: "Un sito speciale ✨",
+    url: "https://lacittapiubelladelmondo.it",
+    siteName: "La città più bella del mondo",
+    images: [
+      {
+        url: "https://lacittapiubelladelmondo.it/cosenza.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Cosenza - La città più bella del mondo",
+      },
+    ],
+    locale: "it_IT",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "La città più bella del mondo",
+    description: "Un sito speciale ✨",
+    images: ["https://lacittapiubelladelmondo.it/cosenza.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -16,7 +50,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="it">
-      <body className={`${montserrat.className} ${kapakana.className}`}>{children}</body>
+      <body className={`${montserrat.className} ${kapakana.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
